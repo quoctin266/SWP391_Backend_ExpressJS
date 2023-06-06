9612da7468d38271f7d1bf620c6b0f0f8255d2c5
Nguyen Quoc Tin <tin.9a1.dt@gmail.com>
2023-06-06 19:16:50 +0700

    sign up, get station, update profile API

--- src/utils/errorCodes.js
+++ src/utils/errorCodes.js
@@ -1,1 +1,6 @@
 export const AUTHENTICATION_FAIL = 100;
+export const DUPLICATE_EMAIL = 101;
+export const DUPLICATE_USERNAME = 102;
+export const REGISTER_FAIL = 103;
+export const FETCHING_FAIL = 104;
+export const UPDATE_FAIL = 105;
