44697dd189aa3cc662df8dfff35422eae53bf1f2
Nguyen Quoc Tin <tin.9a1.dt@gmail.com>
2023-06-23 17:43:55 +0700

    only auto schedule left

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -4,19 +4,9 @@ import AppError from "../custom/AppError";
 import moment from "moment";
 
 const getOrderList = async (req, res, next) => {
-  let status = req.params.status;
-
-  let rows = null;
-  if (status === "all") {
-    [rows] = await connection.execute(
-      "SELECT * FROM `transport_order` JOIN `payment_method` on transport_order.payment_method_id = payment_method.id JOIN `service_package` on transport_order.package_id = service_package.package_id JOIN `customer` on transport_order.customer_id = customer.customer_id"
-    );
-  } else {
-    [rows] = await connection.execute(
-      "SELECT * FROM `transport_order` JOIN `payment_method` on transport_order.payment_method_id = payment_method.id JOIN `service_package` on transport_order.package_id = service_package.package_id JOIN `customer` on transport_order.customer_id = customer.customer_id where status = ?",
-      [status]
-    );
-  }
+  const [rows] = await connection.execute(
+    "SELECT * FROM `transport_order` JOIN `payment_method` on transport_order.payment_method_id = payment_method.id JOIN `service_package` on transport_order.package_id = service_package.package_id JOIN `customer` on transport_order.customer_id = customer.customer_id"
+  );
 
   if (rows.length === 0) {
     throw new AppError(RECORD_NOTFOUND, "No records were found.", 200);
